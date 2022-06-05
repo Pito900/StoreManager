7b2cc45cfb7a0dd20b3d29d7f5d0f6c5a31193f6
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-06-05 17:59:35 -0300

    Mais alguns ajustes para começar os testes

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const salesFromService = require('../services/salesService'); // estamos pegando a função q vem do product service. Essa função pega a base de dados
-const salesValidate = require('../middlewares/salesValidation');
 
 const router = express.Router();
 // a formatação async é necessária (sempre lembrar disso)
-router.get('/', async (_req, res) => {
+const getAllSales = async (_req, res) => {
   const [sales] = await salesFromService.getAllSales();
   res.status(200).json(sales);
-});
-router.get('/', async (_req, res) => {
-  const [sales] = await salesFromService.getAllSales();
-  res.status(200).json(sales);
-});
+};
 
-router.post('/', salesValidate.salesValidation, async (req, res) => {
+const createSale = async (req, res) => {
   try {
       const saleCreated = await salesFromService.createSale(req.body);
       res.status(201).json(saleCreated);
 } catch (error) {
   res.status(500).json({ message: error.message });
 }
-});
+};
 
-router.get('/:id', async (req, res) => {
+const getSalesById = async (req, res) => {
   const { id } = req.params;
   const [sales] = await salesFromService.getSalesById(id);
   if (!sales.length) {
     return res.status(404).json({ message: 'Sale not found' });
   }
   res.status(200).json(sales);
-});
+};
 
-router.put('/:id', salesValidate.salesValidation, async (req, res) => {
+const updateSale = async (req, res) => {
   try {
     const { id } = req.params;
     const [allSales] = await salesFromService.getSalesById(id);
@@ -43,9 +38,9 @@ router.put('/:id', salesValidate.salesValidation, async (req, res) => {
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
-});
+};
 
-router.delete('/:id', async (req, res) => {
+const deleteSaleId = async (req, res) => {
   try {
     const { id } = req.params;
     const [allSales] = await salesFromService.getSalesById(id);
@@ -57,8 +52,13 @@ router.delete('/:id', async (req, res) => {
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
-});
+};
 
 module.exports = {
   router,
+  getAllSales,
+  createSale,
+  getSalesById,
+  updateSale,
+  deleteSaleId,
 };
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productsValidate = require('../middlewares/productValidation'); // validação para o produto (name e quantidade).
+const salesValidate = require('../middlewares/salesValidation');
 
 const router = express.Router();
 
@@ -7,11 +8,18 @@ router.use('/products', require('../controllers/productController').router);
 router.use('/sales', require('../controllers/salesController').router);
 
 const productController = require('../controllers/productController');
+const salesController = require('../controllers/salesController');
 
 router.get('/products', productController.getAllProducts);
 router.get('/products/:id', productController.getProductById);
 router.post('/products', productsValidate.productsValidation, productController.createProducts);
 router.put('/products/:id', productsValidate.productsValidation, productController.updateProducts);
 router.delete('/products/:id', productController.deleteProductId);
+
+router.get('/sales', salesController.getAllSales);
+router.get('/sales/:id', salesController.getSalesById);
+router.post('/sales', salesValidate.salesValidation, salesController.createSale);
+router.put('/sales/:id', salesValidate.salesValidation, salesController.updateSale);
+router.delete('/sales/:id', salesController.deleteSaleId);
 
 module.exports = router;
