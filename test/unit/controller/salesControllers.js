2717d7898206fb4db7170db65db0a3226f357df3
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-06-07 15:41:54 -0300

    Mais testes

--- test/unit/controller/salesControllers.js
+++ test/unit/controller/salesControllers.js
@@ -5,8 +5,8 @@ const salesControllers = require('../../../controllers/salesController');
 
 
 describe('Busca os produtos no BD', () => {
-  describe('Quando o banco de dados de venda ta vazio', () => {
     let req = {}, res = {};
+  describe('Quando o banco de dados de venda ta vazio', () => {
 
     before(() => {
       res.status = sinon.stub().returns(res);
@@ -24,11 +24,86 @@ describe('Busca os produtos no BD', () => {
       expect(res.status.calledWith(404)).to.be.true;
     });
 
-    it('é chamado o send com a mensagem "Sale not found"', async () => {
+    it('Retorna um array vazio', async () => {
       await salesControllers.getAllSales(req, res);
 
       expect(res.send.calledWith([])).to.be.equal(true);
 
     });
   })
+
+  describe('Quando a base de dados n está vazia', ()=> {
+    before(() => {
+      const sales = [
+        {
+          "saleId": 1,
+          "date": "2021-09-09T04:54:29.000Z",
+          "productId": 1,
+          "quantity": 2
+        },
+        {
+          "saleId": 1,
+          "date": "2021-09-09T04:54:54.000Z",
+          "productId": 2,
+          "quantity": 2
+        }
+      ]
+      
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(salesServices, 'getAllSales').resolves(sales);
+    });
+
+    after(() => {
+      salesServices.getAllSales.restore();
+    })
+
+    it('retorna o status 200', async () => {
+        await salesControllers.getAllSales(req, res);
+    
+        expect(res.status.calledWith(200)).to.be.true;
+      });
+    
+      it('deve retornar um objeto com as chaves saleId, date, productId e quantity', async () => {
+        await salesControllers.getAllSales(req, res);
+        const resultado = await salesServices.getAllSales();
+        expect(resultado[0]).to.include.all.keys('saleId', 'date', 'productId', 'quantity');
+      });
+  });
+  describe('procurando venda por id', ()=> {
+    before(() => {
+      req.params = { id: 1 };
+      const sale =  [
+        {
+            "date": "2022-06-07T18:28:38.000Z",
+            "productId": 1,
+            "quantity": 5
+        },
+        {
+            "date": "2022-06-07T18:28:38.000Z",
+            "productId": 2,
+            "quantity": 10
+        }
+    ]
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(salesServices, 'getSalesById').resolves(sale);
+    });
+
+    after(() => {
+      salesServices.getSalesById.restore();
+    })
+
+    it('retorna o status 200', async () => {
+        await salesControllers.getSalesById(req, res);
+    
+        expect(res.status.calledWith(200)).to.be.exist;
+      });
+    
+    //   it('deve retornar um objeto com as chaves saleId, date, productId e quantity', async () => {
+    //     await salesControllers.getAllSales(req, res);
+    //     const resultado = await salesServices.getAllSales();
+    //     expect(resultado[0]).to.include.all.keys('saleId', 'date', 'productId', 'quantity');
+    //   });
+  });
 })
